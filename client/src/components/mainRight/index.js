37724ed51df9052502aed370771956945927caaf
이찬휘 <hwi@ichanhwiui-MacBookAir.local>
2022-04-12 22:39:51 +0900

    [게시판불러오기]

--- client/src/components/mainRight/index.js
+++ client/src/components/mainRight/index.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
-export default function MainRight() {
+const  MainRight = () => {
+
+  const getPostList = async () => {
+    try {
+       const res = await axios.post('http://localhost:1212/products/getlist')
+       .then((res) => {
+         console.log(res.data)
+      })
+    } catch (err) {
+      console.log('fail')
+    }
+  }
+
+  
+
+  useEffect(() => {
+    getPostList();
+  });
+
+
+
   return (
     <div className="container">MainRight</div>
   )
 }
+
+
+
+export default MainRight;
