55c8cfd26addce80b1cd6126dc2e928225e93d04
yongho <yongho6800@gmail.com>
2022-05-02 19:25:23 +0900

    inf prf src bar

--- client/src/components/body/searchPlace/index.js
+++ client/src/components/body/searchPlace/index.js
@@ -8,9 +8,44 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
 import './index.css'
 
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+const minDistance = 10;
+
 const SearchBar = () => {
+
+
+  
+    const [value2, setValue2] = React.useState([20, 37]);
+  
+    const handleChange2 = (event, newValue, activeThumb) => {
+      if (!Array.isArray(newValue)) {
+        return;
+      }
+  
+      if (newValue[1] - newValue[0] < minDistance) {
+        if (activeThumb === 0) {
+          const clamped = Math.min(newValue[0], 100 - minDistance);
+          setValue2([clamped, clamped + minDistance]);
+        } else {
+          const clamped = Math.max(newValue[1], minDistance);
+          setValue2([clamped - minDistance, clamped]);
+        }
+      } else {
+        setValue2(newValue);
+      }
+    };
+  
+
+
+
   return (
     <>
     <div className="serchbarcontainer">
@@ -61,6 +96,18 @@ const SearchBar = () => {
         )}
       />
     </Stack>
+
+    <Box sx={{ width: 300, marginTop: '15px' }}>
+        <Typography gutterBottom>Followers</Typography>
+        <Slider
+            getAriaLabel={() => 'Minimum distance shift'}
+            value={value2}
+            onChange={handleChange2}
+            valueLabelDisplay="auto"
+            getAriaValueText={valuetext}
+            disableSwap
+        />
+    </Box>
     </div>
     </>
   );
