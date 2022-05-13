89cda2838bf58a751bb9ca5748a17d0a390ca1ac
yongho <yongho6800@gmail.com>
2022-05-13 21:20:25 +0900

    workspace/dashboard

---
+++ client/src/components/body/workSpace/Myprd.js
@@ -0,0 +1,80 @@
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
+import { Link } from "react-router-dom";
+
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../../../state/index';
+
+import {
+  getAuth
+} from "firebase/auth";
+
+const Myprd = () => {
+
+  const auth = getAuth();
+  
+
+  const [product, setProduct] = useState();
+
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state)
+  const {loginUser, logoutUser, fbuser, nofbuser} = bindActionCreators(actionCreators, dispatch);
+
+  const getListById = async () => {
+    const uid = auth.currentUser.uid
+    console.log(uid)
+    try {
+       const res = await axios.get('http://localhost:1212/products/getlistbyid', 
+       { params: { authorUid: uid } })
+       .then((res) => { 
+        console.log(res.data);
+        setProduct(res.data);
+        return 0;
+      })
+    } 
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getListById();
+  }, []);
+  
+
+  return (
+    <div className="main_Right_chan" style={{display: 'flex', flexWrap: 'wrap'}}>
+      {product ? product.map(item => {
+        return (
+          <div key={item._id} style={{ margin: '20px'}}>
+            <Link to={`/Detail/${item._id}`} style={{ color: 'black', display: 'flex', flexDirection: 'column', width: '200px', height: '280px', alignItems: 'flex-start' }}>
+              {/* <Link to={`/Detail/${item.name}`} /> */}
+              <div style={{ width: '200px', height: '200px', backgroundColor: 'red' }}>
+                
+                <img className='profile-img' src={item.photo} />
+              </div>
+              <div style={{ display: 'flex', flexDirection: 'column',  backgroundColor:'#fff' }}>
+                <div style={{ fontSize: '20px', }}>
+                  <div>
+                    {item.brand}
+                  </div>
+                </div>
+                <div style={{ fontSize: '20px' }}>
+                  {item.name}
+                </div>
+              </div>
+              <div style={{ fontSize: '14px' }}>
+                {item.point}
+              </div>
+            </Link>
+          </div>
+        )
+      }) : ''}
+    </div> 
+  )
+}
+
+
+
+export default Myprd
--- client/src/components/body/workSpace/Workspace.js
+++ client/src/components/body/workSpace/Workspace.js
@@ -1,80 +1,142 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios'
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { mainListItems, secondaryListItems } from './listItems';
+import Chart from './Chart';
+import Deposits from './Deposits';
+import Orders from './Orders';
+import Myprd from './Myprd';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { actionCreators } from '../../../state/index';
 
-import {
-  getAuth
-} from "firebase/auth";
 
-const Workspace = () => {
+const mdTheme = createTheme();
 
-  const auth = getAuth();
-  
-
-  const [product, setProduct] = useState();
-
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state)
-  const {loginUser, logoutUser, fbuser, nofbuser} = bindActionCreators(actionCreators, dispatch);
-
-  const getListById = async () => {
-    const uid = auth.currentUser.uid
-    console.log(uid)
-    try {
-       const res = await axios.get('http://localhost:1212/products/getlistbyid', 
-       { params: { authorUid: uid } })
-       .then((res) => { 
-        console.log(res.data);
-        setProduct(res.data);
-        return 0;
-      })
-    } 
-    catch (err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-    getListById();
-  }, []);
-  
+function WorkSpace() {
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   return (
-    <div className="main_Right_chan" style={{display: 'flex', flexWrap: 'wrap'}}>
-      {product ? product.map(item => {
-        return (
-          <div key={item._id} style={{ margin: '20px'}}>
-            <Link to={`/Detail/${item._id}`} style={{ color: 'black', display: 'flex', flexDirection: 'column', width: '200px', height: '280px', alignItems: 'flex-start' }}>
-              {/* <Link to={`/Detail/${item.name}`} /> */}
-              <div style={{ width: '200px', height: '200px', backgroundColor: 'red' }}>
-                
-                <img className='profile-img' src={item.photo} />
-              </div>
-              <div style={{ display: 'flex', flexDirection: 'column',  backgroundColor:'#fff' }}>
-                <div style={{ fontSize: '20px', }}>
-                  <div>
-                    {item.brand}
-                  </div>
-                </div>
-                <div style={{ fontSize: '20px' }}>
-                  {item.name}
-                </div>
-              </div>
-              <div style={{ fontSize: '14px' }}>
-                {item.point}
-              </div>
-            </Link>
-          </div>
-        )
-      }) : ''}
-    </div> 
-  )
+    <ThemeProvider theme={mdTheme}>
+      <Box >
+        <CssBaseline />
+        <Box style={{width:'100%', backgroundColor:'#0f0'}}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+           
+              <MenuIcon />
+            
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Dashboard
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </Box>
+       
+          <Box sx={{ display: 'flex' }}>
+          <Divider />
+          <List component="nav">
+            {mainListItems}
+            <Divider sx={{ my: 1 }} />
+            {secondaryListItems}
+          </List>
+          
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              {/* Chart */}
+              <Grid item xs={12} md={8} lg={9}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  <Chart />
+                </Paper>
+              </Grid>
+
+              {/* Recent Deposits */}
+              <Grid item xs={12} md={4} lg={3}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                  <Deposits />
+                </Paper>
+              </Grid>
+
+              {/* Recent Orders */}
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                  <Orders />
+                </Paper>
+              </Grid>
+
+              {/* Recent Orders */}
+              <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                  <Myprd />
+                </Paper>
+              </Grid>
+
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-
-
-export default Workspace
+export default function Dashboard() {
+  return <WorkSpace />;
+}
