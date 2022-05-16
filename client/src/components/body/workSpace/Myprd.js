461ebce4855d4adb926cea0296bbc1b60fde0b96
yongho <yongho6800@gmail.com>
2022-05-16 21:55:55 +0900

    my prd

--- client/src/components/body/workSpace/Myprd.js
+++ client/src/components/body/workSpace/Myprd.js
@@ -26,7 +26,7 @@ const Myprd = () => {
     console.log(uid)
     try {
        const res = await axios.get('http://localhost:1212/products/getlistbyid', 
-       { params: { authorUid: uid } })
+       { params: { uid } })
        .then((res) => { 
         console.log(res.data);
         setProduct(res.data);
