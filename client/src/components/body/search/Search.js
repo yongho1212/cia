5b66cb553b5dd4bab8e4ed08186746cf16f0ee61
yongho <yongho6800@gmail.com>
2022-06-15 20:19:01 +0900

    my chat list

--- client/src/components/body/search/Search.js
+++ client/src/components/body/search/Search.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-function Search (props) {
+import { reauthenticateWithCredential, EmailAuthProvider, deleteUser } from "firebase/auth";
+import {updateProfile} from 'firebase/auth'
+import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../../../state/index';
+
+
+
+const Search = (props) => {
+
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state)
+  const {loginUser, logoutUser, fbuser, nofbuser} = bindActionCreators(actionCreators, dispatch);
+  const navigate = useNavigate();
+
+  const channels = state.auth.state.loginData.joinedChannel
+  const lists = channels.map((chat) => 
+  <li>{chat}</li>
+  )
+
+  console.log(state.auth.state.loginData.joinedChannel)
+
   return (
-    <div>Search {props.text} </div>
+    <div>
+      <h1>My Chat list</h1>
+    {lists}
+    
+    </div>
+    
+    
+    
   );
 } 
 export default Search;
